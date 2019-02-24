da125475fadd58fb9d0a4989ec7330203cd165fb
Brandon Evans <brandon@brandonevans.ca>
2019-02-24 00:36:04 -0700

    Implement BinaryEncodable
    The encoding was verified by decoding a .binarycookies file, then encoding it and writing to a file and diffing the files with colordiff and xxd.
    
    There are some changes that came along with the addition of this conformance in order to get the output matching, like comments and new Flags values.
    
    Speaking of those flags values, there are two that are now codable but the meaning is unknown. I'm assuming that one is the discard flag, but I'm not sure what the other one is or which is which. I'll need to do more testing, because so far I've just been using the main Cookies.binarycookies file on my computer to verify.
    
    This also implements the checksum, which is the sum of every 4th byte in all of the pages.
    
    Addresses #1.

--- Sources/BinaryCookies/BinaryCookies.swift
+++ Sources/BinaryCookies/BinaryCookies.swift
@@ -1,7 +1,7 @@
 import Foundation
 import BinaryCodable
 
-public class BinaryCookies: BinaryDecodable {
+public class BinaryCookies: BinaryCodable {
     public var pages: [Page]
     public var metadata: Any
 
@@ -42,11 +42,33 @@ public class BinaryCookies: BinaryDecodable {
         metadata = try PropertyListSerialization.propertyList(from: plistData, options: [], format: nil)
     }
 
+    public func encode(to encoder: BinaryEncoder) throws {
+        var container = encoder.sequentialContainer()
+
+        try container.encode(sequence: BinaryCookies.magic)
+        try container.encode(Int32(pages.count).bigEndian)
+        for page in pages {
+            try container.encode(Int32(page.totalByteCount).bigEndian)
+        }
+
+        for page in pages {
+            try container.encode(page)
+        }
+
+        let checksum: Int32 = try pages.reduce(0) { try $0 + $1.checksum() }
+        try container.encode(checksum.bigEndian)
+
+        try container.encode(BinaryCookies.footer.bigEndian)
+
+        let plistData = try PropertyListSerialization.data(fromPropertyList: metadata, format: .binary, options: 0)
+        try container.encode(sequence: plistData)
+    }
+
     private static let magic = Data("cook".utf8)
-    private static let footer = 0x071720050000004b
+    private static let footer: Int64 = 0x071720050000004b
 }
 
-public class Page: BinaryDecodable {
+public class Page: BinaryCodable {
     public var cookies: [Cookie]
 
     public required init(from decoder: BinaryDecoder) throws {
@@ -77,47 +99,84 @@ public class Page: BinaryDecodable {
         self.cookies = cookies
     }
 
-    private static let header = 0x00000100
-    private static let footer = 0x00000000
+    public func encode(to encoder: BinaryEncoder) throws {
+        var container = encoder.sequentialContainer()
+
+        try container.encode(Page.header.bigEndian)
+        try container.encode(Int32(cookies.count))
+
+        for index in 0..<cookies.count {
+            let offset = cookiesByteOffset + cookies[0..<index].reduce(0) { $0 + $1.totalByteCount }
+            try container.encode(offset)
+        }
+
+        try container.encode(Page.footer)
+
+        for cookie in cookies {
+            try container.encode(cookie)
+        }
+    }
+
+    private var cookiesByteOffset: Int32 {
+        return Int32(12 + 4 * cookies.count)
+    }
+
+    var totalByteCount: Int32 {
+        return cookiesByteOffset + cookies.reduce(0) { $0 + $1.totalByteCount }
+    }
+
+    func checksum() throws -> Int32 {
+        let data = try BinaryDataEncoder().encode(self)
+        var checksum: Int32 = 0
+        for index in stride(from: 0, to: data.count, by: 4) {
+            checksum += Int32(data[index])
+        }
+        return checksum
+    }
+
+    private static let header: Int32 = 0x00000100
+    private static let footer: Int32 = 0x00000000
 }
 
-public class Cookie: BinaryDecodable {
+public class Cookie: BinaryCodable {
     public var version: Int32
     public var url: String!
     public var name: String!
     public var path: String!
     public var value: String!
-    public let isSecure: Bool
-    public let isHTTPOnly: Bool
+    public var comment: String?
+    public let flags: Flags
     public let creation: Date
     public let expiration: Date
 
-    struct Flags: OptionSet, BinaryDecodable {
-        let rawValue: Int32
+    public struct Flags: OptionSet, BinaryCodable {
+        public let rawValue: Int32
 
-        static let isSecure = Flags(rawValue: 0b1)
-        static let isHTTPOnly = Flags(rawValue: 0b100)
+        public init(rawValue: Int32) {
+            self.rawValue = rawValue
+        }
+
+        static let isSecure   = Flags(rawValue: 1)
+        static let isHTTPOnly = Flags(rawValue: 1 << 2)
+        static let unknown1   = Flags(rawValue: 1 << 3)
+        static let unknown2   = Flags(rawValue: 1 << 4)
     }
 
     public required init(from decoder: BinaryDecoder) throws {
         var container = decoder.sequentialContainer(maxLength: nil)
 
         let size = try container.decode(Int32.self)
-
         version = try container.decode(Int32.self)
-
-        let flags = try container.decode(Flags.self)
-        isSecure = flags.contains(.isSecure)
-        isHTTPOnly = flags.contains(.isHTTPOnly)
-
+        flags = try container.decode(Flags.self)
         let _ = try container.decode(length: 4)
 
         let urlOffset = try container.decode(Int32.self)
         let nameOffset = try container.decode(Int32.self)
         let pathOffset = try container.decode(Int32.self)
         let valueOffset = try container.decode(Int32.self)
+        let commentOffset = try container.decode(Int32.self)
 
-        let footer = try container.decode(Int64.self)
+        let footer = try container.decode(Int32.self)
         guard footer == Cookie.footer else { throw BinaryDecodingError.dataCorrupted(.init(debugDescription: "Invalid cookie footer")) }
 
         let expiration = try container.decode(length: 8).withUnsafeBytes { $0.pointee as TimeInterval }
@@ -128,7 +187,7 @@ public class Cookie: BinaryDecodable {
         // url, name, path, and value aren't in a known order, and because
         // BinaryCodable can't seek to an offset, do a little math to figure out
         // the order and trust that they aren't padded.
-        let offsets: [Int32] = [urlOffset, nameOffset, pathOffset, valueOffset].sorted()
+        let offsets: [Int32] = [urlOffset, nameOffset, pathOffset, valueOffset, commentOffset].sorted()
         for (offset, next) in zip(offsets, offsets.dropFirst() + [size]) {
             let length = Int(next - offset)
 
@@ -148,8 +207,58 @@ public class Cookie: BinaryDecodable {
                 let valueData = try container.decode(length: length)
                 value = String(data: valueData, encoding: .utf8)!
             }
+            else if offset == commentOffset, offset > 0 {
+                let commentData = try container.decode(length: length)
+                comment = String(data: commentData, encoding: .utf8)!
+            }
         }
     }
 
-    private static let footer = 0x0000000000000000
+    public func encode(to encoder: BinaryEncoder) throws {
+        var container = encoder.sequentialContainer()
+
+        try container.encode(totalByteCount)
+        try container.encode(version)
+        try container.encode(flags)
+        try container.encode(Int32(0x00000000))
+
+        let commentOffset = fixedByteSize
+        let urlOffset = commentOffset + Int32(comment?.utf8.count ?? 0)
+        try container.encode(urlOffset)
+        let nameOffset = urlOffset + Int32(url.utf8.count)
+        try container.encode(nameOffset)
+        let pathOffset = nameOffset + Int32(name.utf8.count)
+        try container.encode(pathOffset)
+        let valueOffset = pathOffset + Int32(path.utf8.count)
+        try container.encode(valueOffset)
+        if comment != nil {
+            try container.encode(commentOffset)
+        }
+        else {
+            try container.encode(Int32(0))
+        }
+
+        try container.encode(Cookie.footer)
+
+        let expiration = withUnsafeBytes(of: self.expiration.timeIntervalSinceReferenceDate) { Data($0) }
+        try container.encode(sequence: expiration)
+        let creation = withUnsafeBytes(of: self.creation.timeIntervalSinceReferenceDate) { Data($0) }
+        try container.encode(sequence: creation)
+
+        if let comment = comment {
+            try container.encode(comment, encoding: .utf8, terminator: nil)
+        }
+        try container.encode(url, encoding: .utf8, terminator: nil)
+        try container.encode(name, encoding: .utf8, terminator: nil)
+        try container.encode(path, encoding: .utf8, terminator: nil)
+        try container.encode(value, encoding: .utf8, terminator: nil)
+    }
+
+    private let fixedByteSize: Int32 = 56
+
+    var totalByteCount: Int32 {
+        return fixedByteSize + Int32(comment?.utf8.count ?? 0) + Int32(url.utf8.count) + Int32(name.utf8.count) + Int32(path.utf8.count) + Int32(value.utf8.count)
+    }
+
+    private static let footer: Int32 = 0x00000000
 }
